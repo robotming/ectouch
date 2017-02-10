5cd0b811598b2de0ff30e9c4a0843e9c89310c07
carson <docxcn@gmail.com>
2017-02-10 18:21:52 +0800

    更新静态资源配置

--- app/assets/AppAsset.php
+++ app/assets/AppAsset.php
@@ -9,9 +9,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'static/css/app.css',
+        'css/app.css',
     ];
     public $js = [
-        'static/js/app.js',
+        'js/app.js',
+        'js/main.js',
     ];
 }
