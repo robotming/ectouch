85101b6ad42a6d62aa8150efbb73608b537030a6
carson <docxcn@gmail.com>
2017-02-14 13:44:45 +0800

    增加helper函数

--- app/helpers/common.php
+++ app/helpers/common.php
@@ -1,12 +1,33 @@
 <?php
 
-if (!function_exists('dd')) {
+/**
+ * var_dump & die
+ * @param $data
+ */
+function dd($data)
+{
+    echo '<pre>';
+    print_r($data);
+    echo '</pre>';
+    die();
+}
 
-    function dd($data)
-    {
-        echo '<pre>';
-        print_r($data);
-        echo '</pre>';
-        die();
-    }
+/**
+ * 返回商品图片URL
+ * @param $url
+ * @return string
+ */
+function getImagePath($url)
+{
+    return rtrim(Yii::$app->params['static'], '/') . '/' . ltrim($url, '/');
+}
+
+/**
+ * 返回Data资源URL
+ * @param $url
+ * @return string
+ */
+function getDataPath($url)
+{
+    return rtrim(Yii::$app->params['static'], '/') . '/data/' . ltrim($url, '/');
 }
